48e310eddeabff1874b3f779175e901d703b784c
meatbags <jxburrow@gmail.com>
2018-10-14 19:45:32 +1100

    og fix ?

--- header.php
+++ header.php
@@ -3,17 +3,17 @@
 <html lang="en" prefix="og: http://ogp.me/ns#">
 <head>
 	<title>Closed On Monday</title>
-	<?php wp_head(); ?>
 	<meta charset="utf-8">
-	<meta name="description" content="A virtual webgl gallery packed with exciting art!">
+	<meta name="description" content="Closed On Monday is an online 3D space that hosts monthly exhibitions.">
   <meta name="keywords" content="virtual gallery, online gallery, webgl, 3D">
   <meta name="author" content="http://xavierburrow.com">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<meta property="og:url" content="http://closedonmondaygallery.com/">
 	<meta property="og:title" content="Closed On Mondays">
-	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.jpg">
+	<meta property="og:image" content="http://closedonmondaygallery.com/wp-content/themes/galleria/img/og-image.jpg">
 	<meta property="og:site_name" content="Closed On Mondays">
-	<meta property="og:description" content="A virtual gallery.">
+	<meta property="og:description" content="Closed On Monday is an online 3D space that hosts monthly exhibitions.">
+	<?php wp_head(); ?>
 	<link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
 	<script type="text/javascript">
 	//<![CDATA[
--- index.php
+++ index.php
@@ -12,4 +12,5 @@
 ?>
 
 <?php wp_footer(); ?>
-</body></html>
+</body>
+</html>
