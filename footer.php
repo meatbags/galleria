26c2b98ae1bcc3a14431501b05f2532395328d5e
meatbags <jxburrow@gmail.com>
2018-10-16 18:00:18 +1100

    fix forms

---
+++ footer.php
@@ -0,0 +1,41 @@
+<script type='text/javascript'>
+  var buttons = document.querySelectorAll('.newsletter-submit-form');
+  for (var i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      var el = evt.currentTarget;
+      var parent = el.parentNode;
+      var input = parent.querySelector('input');
+      var email = encodeURIComponent(input.value);
+      if (email != '') {
+        var baseUrl = 'https://script.google.com/macros/s/AKfycbxpOII5qXk8NJeTZPTLyUfTZ2OAqzg6a5aQJYuQYrvG3jUPjUjp/exec';
+        var data = '?comFormField=' + email;
+        var req = new XMLHttpRequest();
+
+        // sending box
+        var thanks = document.querySelectorAll('.newsletter-thanks');
+        for (var j=0; j<thanks.length; ++j) {
+          thanks[j].classList.add('active');
+        }
+
+        req.open('GET', baseUrl + data, true);
+        req.onload = function() {
+          if (req.status >= 200 && req.status < 400) {
+            var text = document.querySelector('.newsletter-thanks-message').innerHTML;
+            for (var j=0; j<thanks.length; ++j) {
+              thanks[j].innerHTML = text;
+            }
+            input.value = '';
+            // var resp = req.responseText;
+          } else {
+            //console.log("Server Error", req);
+          }
+        };
+        req.onerror = function(err) {
+          //console.log("Client Error", err);
+        };
+        req.send();
+      }
+    });
+  }
+</script>
--- page-footer.php
+++ page-footer.php
@@ -1,7 +1,18 @@
 <div class='page-footer'>
   <div class='page-footer__inner'>
     <div class='content'>
-      <a href='https://www.instagram.com/closed_on_monday_gallery/' target='_blank'> insta </a>/<a href='mailto:closedonmondaygallery@gmail.com'> email </a>
+      <div class='socmed'>
+        <a href='https://www.instagram.com/closed_on_monday_gallery/' target='_blank'> insta </a>/<a href='mailto:closedonmondaygallery@gmail.com'> email </a>
+      </div>
+      <div class='newsletter'>
+        <div class='newsletter-title'>Sign up for our newsletter</div>
+        <form class='newsletter-form'>
+          <input type="text" name="comFormField" placeholder="Your Email"/>
+          <button type="submit" class='newsletter-submit-form'>Submit</button>
+        </form>
+        <div class='newsletter-thanks'>sending...</div>
+        <div class='newsletter-thanks-message'>thanks :)</div>
+      </div>
     </div>
   </div>
 </div>
