cf65733c4fe08bef06bcc81879d6578ecea24b35
meatbags <jxburrow@gmail.com>
2019-01-19 09:21:32 +1100

    lighting, spot

--- js/modules/world/lighting.js
+++ js/modules/world/lighting.js
@@ -21,13 +21,14 @@ class Lighting {
     this.scene.add(this.sky);
 
     // lights [6]
-    this.lights = {point: {}, ambient: {}, directional: {}, hemisphere: {}};
+    this.lights = {point: {}, ambient: {}, directional: {}, hemisphere: {}, spot: {}};
     this.lights.point.a = new THREE.PointLight(0xffffff, 1, 24, 2);
     this.lights.point.b = new THREE.PointLight(0xffffff, 1, 32, 2);
     this.lights.point.c = new THREE.PointLight(0xffffff, 1, 20, 2);
     this.lights.ambient.a = new THREE.AmbientLight(0xffffff, 0.3);
     this.lights.directional.a = new THREE.DirectionalLight(0xffffff, 0.5);
     this.lights.hemisphere.a = new THREE.HemisphereLight(0x0, 0x0000ff, 0.25);
+    this.lights.spot.a = new THREE.SpotLight(0xffffff, 1, 32, Math.PI / 3, 0.25);
 
     // apply exhibition settings
     this.reloadExhibition();
@@ -39,15 +40,22 @@ class Lighting {
     this.lights.point.b.position.set(0, 10, -4);
     this.lights.point.c.position.set(26, 10, 6);
     this.lights.directional.a.position.set(-1, 1.5, -1);
+    this.lights.spot.a.position.set(0, 10, 14);
+    this.lights.spot.a.target.position.set(-4, 0, 6);
 
     // default light settings
     this.lights.point.a.distance = 24;
     this.lights.hemisphere.a.intensity = 0.25;
-
+    this.lights.ambient.a.intensity = 0.3;
+    
     // add all lights
     Object.keys(this.lights).forEach(type => {
-      Object.keys(this.lights[type]).forEach(light => {
-        this.scene.add(this.lights[type][light]);
+      Object.keys(this.lights[type]).forEach(key => {
+        const light = this.lights[type][key];
+        this.scene.add(light);
+        if (light.isSpotLight) {
+          this.scene.add(light.target);
+        }
       });
     });
 
@@ -58,10 +66,10 @@ class Lighting {
         case 'TIYAN':
           // remove lights outside temp exhibition region
           this.scene.remove(this.lights.directional.a);
-          this.scene.remove(this.lights.ambient.a);
+          this.scene.remove(this.lights.point.b);
 
           // reposition & colour lights
-          //this.lights.ambient.a.intensity = 0.05;
+          this.lights.ambient.a.intensity = 0.1;
           this.lights.point.b.position.set(6, 10, 14);
           this.lights.point.a.position.set(2, 9, 18);
           this.lights.point.a.distance = 26;
@@ -70,6 +78,8 @@ class Lighting {
           break;
         case 'JACK_DE_LACY':
         default:
+          this.scene.remove(this.lights.spot.a);
+          this.scene.remove(this.lights.spot.a.target);
           break;
       }
     }
