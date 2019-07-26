eef68a592dcd1fed6916bc60255c136faa7a7f65
meatbags <jxburrow@gmail.com>
2019-07-26 12:11:08 +1000

    phone testing

--- js/utils/is_safari_mobile.js
+++ js/utils/is_safari_mobile.js
@@ -1,10 +1,11 @@
 /** Detect Safari on mobile */
 
 const IsSafariMobile = () => {
-  var ua = window.navigator.userAgent;
-  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-  var webkit = !!ua.match(/WebKit/i);
-  return (iOS && webkit && !ua.match(/CriOS/i));
+  //var ua = window.navigator.userAgent;
+  //var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  //var webkit = !!ua.match(/WebKit/i);
+  //return (iOS && webkit && !ua.match(/CriOS/i));
+  return /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
 }
 
 export default IsSafariMobile;
